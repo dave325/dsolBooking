017f2e91d5aae8a0efe2c95e8420c3c9a8e2e8a0
Kelvin Lui <kelvin97lui@gmail.com>
2019-04-19 13:01:01 -0400

    add branch 3 & 4 into dummy_data.sql

--- dummy_data.sql
+++ dummy_data.sql
@@ -55,3 +55,53 @@ Insert into room_container (c_id, r_id, t_id, container_number) values (92, 902,
 Insert into time_table (t_id, start_time, end_time) values (92, 20190902033500, 20190902043000);
 Insert into reservation(res_id, c_id, t_id, modified_by, created_at, modified_at, created_by, company_name,email,attendance,notes) values (92, 92, 92, "CK", 20190901103000, NULL, "CK", "CK's Company", "CK123@gmail.com", 3, "Need more beer");
 
+
+-- ********************************************************************************************************
+-- Branch 3:
+
+Insert into branch (b_id, b_name) values (3000, "Branch 3");
+Insert into branch_schedule(bs_id, b_id, open_time, close_time) values (3, 3000, 20190901123000, 20200601123000);
+Insert into room (r_id, room_number, b_id) values (700, 1, 3000);
+Insert into room_container (c_id, r_id, t_id, container_number) values (70, 700, 1231, 1);
+Insert into time_table (t_id, start_time, end_time) values (7, 20190901123000, 20190901033000);
+Insert into reservation(res_id, c_id, t_id, modified_by, created_at, modified_at, created_by, company_name,email,attendance,notes) values (7, 70, 7, "Eve", 20190901103000, NULL, "Eve", "Eve's Company", "Eve123@gmail.com",4, "Need more phones");
+
+-- Same Branch (id: 3000)
+-- Same branch_schedule (since same branch)
+
+Insert into room (r_id, room_number, b_id) values (701, 2, 3000);
+Insert into room_container (c_id, r_id, t_id, container_number) values (71, 701, 1231, 1);
+Insert into time_table (t_id, start_time, end_time) values (71, 20190902123000, 20190902033000);
+Insert into reservation(res_id, c_id, t_id, modified_by, created_at, modified_at, created_by, company_name,email,attendance,notes) values (71, 71, 71, "Bernie", 20190901103000, NULL, "Bernie", "Bernie's Company", "bernie123@gmail.com", 2, "Need more strawberries");
+
+-- Same Branch (id: 3000)
+-- Same branch_schedule (since same branch)
+Insert into room (r_id, room_number, b_id) values (702, 2, 3000);
+Insert into room_container (c_id, r_id, t_id, container_number) values (72, 702, 4533, 1);
+Insert into time_table (t_id, start_time, end_time) values (72, 20190902033500, 20190902043000);
+Insert into reservation(res_id, c_id, t_id, modified_by, created_at, modified_at, created_by, company_name,email,attendance,notes) values (72, 72, 72, "Claire", 20190901103000, NULL, "Claire", "Claire's Company", "Claire123@gmail.com", 3, "Need more oranges");
+
+-- ********************************************************************************************************
+-- Branch 4:
+Insert into branch (b_id, b_name) values (4000, "Branch 4");
+Insert into branch_schedule(bs_id, b_id, open_time, close_time) values (4, 4000, 20190901123000, 20200601123000);
+Insert into room (r_id, room_number, b_id) values (400, 1, 4000);
+Insert into room_container (c_id, r_id, t_id, container_number) values (40, 400, 1231, 1);
+Insert into time_table (t_id, start_time, end_time) values (4, 20190901123000, 20190901033000);
+Insert into reservation(res_id, c_id, t_id, modified_by, created_at, modified_at, created_by, company_name,email,attendance,notes) values (4, 40, 4, "Betty", 20190901103000, NULL, "Betty", "Betty's Company", "Betty123@gmail.com",4, "Need more chargers");
+
+-- Same Branch (id: 4000)
+-- Same branch_schedule (since same branch)
+Insert into room (r_id, room_number, b_id) values (401, 2, 4000);
+Insert into room_container (c_id, r_id, t_id, container_number) values (41, 401, 1231, 1);
+Insert into time_table (t_id, start_time, end_time) values (41, 20190902123000, 20190902033000);
+Insert into reservation(res_id, c_id, t_id, modified_by, created_at, modified_at, created_by, company_name,email,attendance,notes) values (41, 41, 41, "Annie", 20190901103000, NULL, "Annie", "Annie's Company", "Annie123@gmail.com", 2, "Need more butter");
+
+-- Same Branch (id: 4000)
+-- Same branch_schedule (since same branch)
+Insert into room (r_id, room_number, b_id) values (402, 2, 4000);
+Insert into room_container (c_id, r_id, t_id, container_number) values (42, 402, 4533, 1);
+Insert into time_table (t_id, start_time, end_time) values (42, 20190902033500, 20190902043000);
+Insert into reservation(res_id, c_id, t_id, modified_by, created_at, modified_at, created_by, company_name,email,attendance,notes) values (42, 42, 42, "Robert", 20190901103000, NULL, "Robert", "Robert's Company", "Robert123@gmail.com", 3, "Need more beans");
+
+
